2ca2bc76a229465cd7b29db85d35d6eb5e2ea05a
Anna Eklund <anna.eklund@yahoo.se>
2025-10-07 15:14:59 +0200

    Format file

--- src/features/courses/AktivityStudent/component/ActivityStudent.tsx
+++ src/features/courses/AktivityStudent/component/ActivityStudent.tsx
@@ -5,19 +5,21 @@ import { fetchWithToken } from "../../../shared/utilities";
 import CreateActivityModal from "../../ActivityModals/CreateActivityModal";
 import { FilePlus2 } from "lucide-react";
 
-interface Activity{
-    id: string;
-    name: string;
-    description: string;
-    startTime: string;
-    endTime: string;
-    type: string;
+interface Activity {
+  id: string;
+  name: string;
+  description: string;
+  startTime: string;
+  endTime: string;
+  type: string;
 }
-interface ActivityStudentProps{
-    moduleId: string;
+interface ActivityStudentProps {
+  moduleId: string;
 }
 
-export default function ActivityStudent({moduleId}:ActivityStudentProps):ReactElement {
+export default function ActivityStudent({
+  moduleId,
+}: ActivityStudentProps): ReactElement {
   const [activities, setActivities] = useState<Activity[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
@@ -25,76 +27,78 @@ export default function ActivityStudent({moduleId}:ActivityStudentProps):ReactEl
   const [modalOpen, setModalOpen] = useState(false);
   const userRole = "Teacher"; // WIP
 
+  const fetchActivities = useCallback(async () => {
+    setLoading(true);
 
-    const fetchActivities = useCallback(async () => {
-          setLoading(true);
-
-      try {
-        const data= await fetchWithToken<any>(
-          `https://localhost:7213/api/activity/${moduleId}/activities`
-        );
-          setActivities(data);
-        } catch {
-        setError("Inga aktiviteter för denna modul.");
-      } finally {
-        setLoading(false); 
-      }
+    try {
+      const data = await fetchWithToken<any>(
+        `https://localhost:7213/api/activity/${moduleId}/activities`
+      );
+      setActivities(data);
+    } catch {
+      setError("Inga aktiviteter för denna modul.");
+    } finally {
+      setLoading(false);
+    }
   }, [moduleId]);
 
-    useEffect(() => {
+  useEffect(() => {
     fetchActivities();
   }, [fetchActivities]);
 
-const formatDateTime = (dateString: string) =>
-  new Date(dateString).toLocaleString("sv-SE", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: false
-  }).replace(',', '');
+  const formatDateTime = (dateString: string) =>
+    new Date(dateString)
+      .toLocaleString("sv-SE", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+      })
+      .replace(",", "");
 
   const renderActivities = () => {
     return (
-              <ul className="activity-list">
-          {activities.map((activity)=>(
-            <li key={activity.id} className="activity-item">
-              <h4 className="activity-name"><span className="activity-type">{activity.type}</span>{activity.name}</h4>
-              {/*<p className="activity-description">{activity.description}</p>*/}
-              <p className="activity-dates">
-               {formatDateTime(activity.startTime)} – {formatDateTime(activity.endTime)}
-              </p>
-              
-            </li>
-          ))}
+      <ul className="activity-list">
+        {activities.map((activity) => (
+          <li key={activity.id} className="activity-item">
+            <h4 className="activity-name">
+              <span className="activity-type">{activity.type}</span>
+              {activity.name}
+            </h4>
+            {/*<p className="activity-description">{activity.description}</p>*/}
+            <p className="activity-dates">
+              {formatDateTime(activity.startTime)} –{" "}
+              {formatDateTime(activity.endTime)}
+            </p>
+          </li>
+        ))}
+      </ul>
+    );
+  };
 
-        </ul>
-    )
-  }
-
-    const renderNoActivities = () => {
-    return <p className="no-activities">Inga aktiviteter för denna modul.</p>
-  }
+  const renderNoActivities = () => {
+    return <p className="no-activities">Inga aktiviteter för denna modul.</p>;
+  };
 
   if (loading) return <p className="loading">Laddar...</p>;
   if (error) return <p className="error">{error}</p>;
 
-      return (
-        <section>
-       <div className="activity-list-container">
-        <h3>Aktiviteter</h3>      
+  return (
+    <section>
+      <div className="activity-list-container">
+        <h3>Aktiviteter</h3>
         <ul className="activity-list">
-          {activities.length === 0 ?
-            renderNoActivities() : renderActivities()}
+          {activities.length === 0 ? renderNoActivities() : renderActivities()}
         </ul>
         {userRole === "Teacher" ? (
           <div>
             <button
               className="create-activity-btn"
-             onClick={() => setModalOpen(true)}
+              onClick={() => setModalOpen(true)}
             >
-              <FilePlus2 size={16}/> Lägg till aktivitet
+              <FilePlus2 size={16} /> Lägg till aktivitet
             </button>
             <CreateActivityModal
               moduleId={moduleId}
@@ -102,14 +106,13 @@ const formatDateTime = (dateString: string) =>
               open={modalOpen}
               onClose={() => setModalOpen(false)}
               onActivityCreated={() => {
-                fetchActivities(); 
+                fetchActivities();
               }}
               userRole={userRole}
             />
-        </div>
-      ) : null}
-       </div>
-
-       </section>
-      )
-};
+          </div>
+        ) : null}
+      </div>
+    </section>
+  );
+}
