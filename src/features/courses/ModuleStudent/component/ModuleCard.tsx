153506bb677ecf5355efe940be3cdc21ca9ae99a
Diana Todorova <diana.n.todorova@gmail.com>
2025-10-07 16:06:28 +0200

    fixing bug i delete

--- src/features/courses/ModuleStudent/component/ModuleCard.tsx
+++ src/features/courses/ModuleStudent/component/ModuleCard.tsx
@@ -15,18 +15,26 @@ interface ModuleCardProps {
 export function ModuleCard({ module, onModuleDeleted, onEdit }: ModuleCardProps): ReactElement {
   const [open, setOpen] = useState(false);
 
-  const handleDelete = async () => {
-    if (window.confirm("Vill du verkligen ta bort denna modul?")) {
-      try {
-        await fetchWithToken(`https://localhost:7213/api/module/${module.id}`, {
-          method: "DELETE",
-        });
-        if (onModuleDeleted) onModuleDeleted();
-      } catch {
-        alert("Kunde inte ta bort modulen.");
-      }
+const handleDelete = async () => {
+  if (window.confirm("Vill du verkligen ta bort denna modul?")) {
+    try {
+      await fetchWithToken(`https://localhost:7213/api/module/${module.id}`, {
+        method: "DELETE",
+      }).catch((err) => {
+        if (
+          err instanceof SyntaxError &&
+          err.message.includes("Unexpected end of JSON input")
+        ) {
+          return;
+        }
+        throw err;
+      });
+      if (onModuleDeleted) onModuleDeleted();
+    } catch {
+      alert("Kunde inte ta bort modulen.");
     }
-  };
+  }
+};
 
   const handleClick = () => {
     setOpen(!open);
